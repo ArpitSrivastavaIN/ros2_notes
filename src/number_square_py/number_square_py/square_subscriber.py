fc48453728f81fa20b7f7976de152f569686b12a
ArpitSrivastavaIN <work.arpit.sri@gmail.com>
2025-05-17 16:27:59 +0530

    Week1 Commit

--- src/number_square_py/number_square_py/square_subscriber.py
+++ src/number_square_py/number_square_py/square_subscriber.py
@@ -1,27 +1,40 @@
-import rclpy
-from rclpy.node import Node
-from std_msgs.msg import Int32
+import rclpy #Importing the ros client library for python
+from rclpy.node import Node # Importing Node class from rclpy
+from std_msgs.msg import Int32 #Importing the standard message type 32-bit integer
 
+#Creaitng a node named SquareSubscriber 
 class SquareSubscriber(Node):
+
     def __init__(self):
+        # Creates a node named square_subscriber
         super().__init__('square_subscriber')
+        """
+        Creating a subscription of
+        Topic : number
+        message type : Int32
+        queue size : 10"""
         self.subscription = self.create_subscription(
             Int32,
             'number',
             self.listener_callback,
             10
         )
-        self.subscription
+        self.subscription # Prevent unused variable warning
 
+    # Function to square the published data and log that info
     def listener_callback(self, msg):
         square = msg.data ** 2
         self.get_logger().info(f"Recieved: {msg.data}, Square : {square}")
 
 def main(args = None):
+    #Initializing the ros library
     rclpy.init(args=args)
+    #Creating the subscriber
     square_subscriber = SquareSubscriber()
+    #Spin the node to keep it alive
     rclpy.spin(square_subscriber)
 
+    #Clean up resources once node is closed
     square_subscriber.destroy_node()
     rclpy.shutdown()
 
